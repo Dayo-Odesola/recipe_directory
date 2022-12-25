8fa8bfe791bab317630483b7a8deee5f2db65972
Dayo-Odesola <dayoodesola2001@gmail.com>
2022-12-25 22:54:27 +0100

    added serchbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,10 @@ import { Link } from 'react-router-dom'
 
 // styles
 import './Navbar.css'
+import './Searchbar'
+
+// components
+import Searchbar from './Searchbar'
 
 export default function Navbar() {
   return (
@@ -10,6 +14,8 @@ export default function Navbar() {
         <Link to="/" className='brand'>
           <h1>Cooking ninja</h1>
         </Link>
+
+        <Searchbar />
         
 
         <Link to="/create">Create Recipe</Link>
